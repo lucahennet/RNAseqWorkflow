8eec566faef1826066af0ee529de79e8fffd035c
Luca Hennet <lhennet@login8.hpc.binf.unibe.ch>
2025-01-30 19:01:25 +0100

    error corrections and improvements

--- 08_qualimap.sh
+++ 08_qualimap.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --job-name=qualimap
 #SBATCH --cpus-per-task=6
-#SBATCH --mem-per-cpu=2G
-#SBATCH --time=00:20:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=01:00:00
 #SBATCH --partition=pibu_el8
 #SBATCH --output=/data/users/lhennet/RNAseqWorkflow/log/qualimap_%J.out
 #SBATCH --error=/data/users/lhennet/RNAseqWorkflow/log/qualimap_%J.err
-#SBATCH --array=1
+#SBATCH --array=1-16
 
 # setup of the environment and the directories
 WORKDIR="/data/users/${USER}/RNAseqWorkflow"
@@ -15,9 +15,12 @@ LOGDIR="$WORKDIR/log"
 OUTDIR="$WORKDIR/qualimap"
 SAMPLELIST="$WORKDIR/metadata/samplelist.tsv"
 REFGENOME="$WORKDIR/reference_genome"
+CONTAINER_PATH="/containers/apptainer/multiqc-1.19.sif"
+MULTIQC_OUTDIR="$WORKDIR/multiqc_qualimap"
 
 mkdir -p $OUTDIR
 mkdir -p $LOGDIR
+mkdir -p $MULTIQC_OUTDIR
 
 module load Qualimap/2.3-foss-2021a-R-4.3.2
 
@@ -43,4 +46,9 @@ qualimap rnaseq \
     -outfile "${SAMPLE}" \
     -outformat "HTML" \
     -p strand-specific-reverse \
-    --java-mem-size=12G >& "$OUTDIR/${SAMPLE}_qualimap.log"
+    --java-mem-size=40G >& "$OUTDIR/${SAMPLE}_qualimap.log"
+
+# Generate MultiQC report after all jobs are done
+if [ "$SLURM_ARRAY_TASK_ID" -eq 16 ]; then
+    apptainer exec $CONTAINER_PATH multiqc $OUTDIR -o $MULTIQC_OUTDIR
+fi
